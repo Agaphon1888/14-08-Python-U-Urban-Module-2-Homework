982285a2fee0184af59fade8aab682538259db54
Agaphon1888 <egorov2000@inbox.ru>
2024-08-24 18:21:08 +0500

    Домашние работы по модулю 2. Добавил задание module_2_2.

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+# Домашняя работа по уроку "Стиль кода часть II. Цикл While."
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+item = 0
+while item < len(my_list):
+    if my_list[item] < 0:
+        break
+    if my_list[item] > 0:
+        print(my_list[item])
+    item += 1
