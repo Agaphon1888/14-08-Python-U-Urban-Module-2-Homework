4d16ca534b7d61ad67b428985f795e10105ad95a
Agaphon1888 <egorov2000@inbox.ru>
2024-08-28 17:36:08 +0500

    Добавил задание module_2_hard

---
+++ module_2_hard.py
@@ -0,0 +1,18 @@
+# Дополнительное практическое задание по модулю: "Основные операторы".
+
+import random
+
+numbers = [3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+rand_numbers = random.choice(numbers)
+print('Случайное число в левой вставке: ', rand_numbers)
+
+def generate_password(rand_numbers):
+    result = ""
+    for i in range(1, rand_numbers):
+        for j in range(i+1, rand_numbers+1):
+            if rand_numbers % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+
+result = generate_password(rand_numbers)
+print('Пароль:', result)
